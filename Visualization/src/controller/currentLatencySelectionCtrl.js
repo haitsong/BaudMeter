03fdcb6d66ac07183924b392497a1a29f8750b56
Chi Wang <alex.chi.wang@gmail.com>
2015-08-20 21:52:04 -0700

    fix angular view not update during d3 event

--- Visualization/src/controller/currentLatencySelectionCtrl.js
+++ Visualization/src/controller/currentLatencySelectionCtrl.js
@@ -5,13 +5,23 @@ angular.module('d3Charts')
         $scope.countyGB = latencyDataService.GetCountyGB1999();
         $scope.coId = latencyDataService.GetCoId();
 
-        eventService.register('selectNewCountyLatency', function (obj) {
-            $scope.countyGB = obj;
-            latencyDataService.SetCountyGB1999(obj);
-        });
+        $scope.updateCountyGB = function(obj){
+            // this event is trigger by D3 component, Angular won't be aware of it, so need to use apply to tell Angular the Model is change, need refresh UI.
+            $scope.$apply(function(){
+                $scope.countyGB = obj;
+                latencyDataService.SetCountyGB1999(obj);
+            });
+        };
 
-        eventService.register('selectNewCo', function (obj) {
-            $scope.coId = obj;
-            latencyDataService.SetCoId(obj);
-        })
+        $scope.updateCoId = function(obj) {
+           // $scope.$apply(function(){
+            // If this function is trigger by angular behavior, there is no need to use apply, since Angular will do it for you.
+                $scope.coId = obj;
+                latencyDataService.SetCoId(obj)
+            //});
+        };
+
+        eventService.register('selectNewCountyLatency', $scope.updateCountyGB);
+
+        eventService.register('selectNewCo', $scope.updateCoId);
     }]);
