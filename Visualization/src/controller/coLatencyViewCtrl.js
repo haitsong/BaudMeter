8611a06a56684415550a748a926b3d752aa50599
Chi Wang <alex.chi.wang@gmail.com>
2015-08-29 20:53:48 -0700

    enhance central offic select view

--- Visualization/src/controller/coLatencyViewCtrl.js
+++ Visualization/src/controller/coLatencyViewCtrl.js
@@ -20,7 +20,7 @@ angular.module('d3Charts')
                         Id : 'Co2',
                         OfficeIp : '201.5.74.52',
                         InComingQps : 4075,
-                        Latency50 : 25.3,
+                        Latency50 : 57.3,
                         Latency75 : 23.3,
                         Latency95 : 20.1
                     },
@@ -28,17 +28,15 @@ angular.module('d3Charts')
                         Id : 'Co3',
                         OfficeIp : '201.5.75.23',
                         InComingQps : 4075,
-                        Latency50 : 25.3,
+                        Latency50 : 48.5,
                         Latency75 : 23.3,
                         Latency95 : 20.1
-                    }
-                ],
-                [
+                    },
                     {
-                        Name : 'Co4',
+                        Id : 'Co4',
                         OfficeIp : '203.5.71.22',
                         InComingQps : 9985,
-                        Latency50 : 25.3,
+                        Latency50 : 22.7,
                         Latency75 : 23.3,
                         Latency95 : 20.1
                     },
@@ -52,6 +50,32 @@ angular.module('d3Charts')
                     },
                     {
                         Id : 'Co6',
+                        OfficeIp : '203.5.75.23',
+                        InComingQps : 10075,
+                        Latency50 : 65.3,
+                        Latency75 : 53.3,
+                        Latency95 : 40.1
+                    }
+                ],
+                [
+                    {
+                        Name : 'Co1',
+                        OfficeIp : '203.5.71.22',
+                        InComingQps : 9985,
+                        Latency50 : 25.3,
+                        Latency75 : 23.3,
+                        Latency95 : 20.1
+                    },
+                    {
+                        Id : 'Co2',
+                        OfficeIp : '203.5.74.52',
+                        InComingQps : 875,
+                        Latency50 : 44.3,
+                        Latency75 : 37.3,
+                        Latency95 : 26.1
+                    },
+                    {
+                        Id : 'Co3',
                         OfficeIp : '203.5.75.23',
                         InComingQps : 10075,
                         Latency50 : 65.3,
